ffe6e5f8422894ed1006d3e73e04224f127b4685
FrankDeng <frankdeng322@gmail.com>
2021-01-24 15:46:22 +0800

    Update get_account_asset_valuation.py

--- example/account/get_account_asset_valuation.py
+++ example/account/get_account_asset_valuation.py
@@ -1,8 +1,8 @@
 from huobi.client.account import AccountClient
 from huobi.constant import *
 
-account_client = AccountClient(api_key=g_api_key,
-                               secret_key=g_secret_key)
+account_client = AccountClient(api_key="db8f342a-3540b528-hrf5gdfghe-5e793",
+                               secret_key="366684d9-94c5fdf7-ad3b02a0-446bf")
 
 account_type = "spot"
 asset_valuation = account_client.get_account_asset_valuation(account_type=account_type, valuation_currency="usd")
