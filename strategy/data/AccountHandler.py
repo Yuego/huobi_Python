b4e0f1fe1eda762b84daf64fe31bb178bc6181b1
FrankDeng <frankdeng322@gmail.com>
2021-01-31 18:49:53 +0800

    Update AccountHandler.py

--- strategy/data/AccountHandler.py
+++ strategy/data/AccountHandler.py
@@ -3,11 +3,8 @@ from huobi.client.margin import MarginClient
 import pandas as pd
 from TradeHandler import TradeHandler
 from huobi.constant import *
-from huobi.constant import *
-from huobi.constant import *
-
-# get accounts
 from huobi.utils import *
+import sqlite3
 
 
 class AccountHandler:
@@ -21,6 +18,8 @@ class AccountHandler:
         self.balance = pd.DataFrame(columns=['account', 'currency', 'type', 'balance'])
         self.margin = {}
         self.th = TradeHandler()
+        self.conn = sqlite3.connect('market.db', timeout=10)
+        self.open_order = 0
 
     def get_accounts(self):
         self.accounts = self.account_client.get_accounts()
@@ -43,8 +42,10 @@ class AccountHandler:
                     pd.Series([account_id, item.currency, item.type, float(item.balance)],
                               index=['account', 'currency', 'type', 'balance']),
                     ignore_index=True)
+        self.balance = self.balance.drop_duplicates()
 
     def update_balance(self):
+        self.balance = pd.DataFrame(columns=['account', 'currency', 'type', 'balance'])
         for item in self.accounts:
             self.get_balance(item.id)
         self.balance = self.balance.loc[self.balance['balance'] != 0].reset_index(drop=True)
@@ -59,6 +60,21 @@ class AccountHandler:
         list_obj = self.account_client.get_account_ledger(account_id=account_id)
         LogInfo.output_list(list_obj)
 
+    def update_history_trades(self, symbol):
+        table = symbol + "_trade_log"
+        self.th.get_match_result(symbol)
+        data = self.th.trade_log.copy()
+        c = self.conn.cursor()
+        # get the count of tables with the name
+        c.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='" + table + "'")
+        # #if the count is 1, then table exists
+        if c.fetchone()[0] == 1:
+            last_data = pd.read_sql('select * from ' + table, self.conn)
+            last_data = last_data.append(data).drop_duplicates().sort_values(by='Time', ascending=True)
+            last_data.sort_values(by='Time', ascending=True).to_sql(table, self.conn, if_exists='replace', index=False)
+        else:
+            data.sort_values(by='Time', ascending=True).to_sql(table, self.conn, if_exists='replace', index=False)
+
     def adjust_position(self, account, currency, target, last_price):
         balance = self.balance
         current_balance = balance[(balance['account'] == account) & (balance['currency'] == currency)]
@@ -67,20 +83,30 @@ class AccountHandler:
         else:
             current_position = 0.0
 
-        if current_position < target*0.95:
+        if current_position < target * 0.95:
             amt = (target - current_position) * last_price
             od = OrderType.BUY_MARKET
-        elif current_position > target*1.05:
-            amt = current_position - target
+            self.place_market_order(currency, account, od, amt)
+        elif current_position > target * 1.05 + 0.00005:
+            amt = round(current_position - target-0.00001,5)
             od = OrderType.SELL_MARKET
+            print(amt)
+            self.place_market_order(currency, account, od, amt)
 
-        print(amt)
-        print(od)
-        self.th.create_order_market(currency + "usdt", account, od, round(amt,6))
+    def place_market_order(self, currency, account, od, amt):
+        try:
+            self.th.batch_cancel(account)
+            order_id = self.th.create_order_market(currency + "usdt", account, od, round(amt, 6))
+            self.open_order = order_id
+        except:
+            print("CANNOT PLACE ORDER!!!!!")
 
+    def check_latest_order(self):
+        if self.open_order > 0:
+            return self.th.get_order(self.open_order)
 
-ac = AccountHandler()
-ac.get_accounts()
-ac.update_asset()
-ac.update_balance()
-ac.get_cross_margin_account()
+# ac = AccountHandler()
+# ac.get_accounts()
+# ac.update_asset()
+# ac.update_balance()
+# ac.get_cross_margin_account()
